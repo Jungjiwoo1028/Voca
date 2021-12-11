1defb26b460f299df3fdc1d2005d6d18d02df9f1
jungjiwoo <madeinkorea1028@gmail.com>
2021-12-11 12:44:51 +0900

    feat: Add redux-persist

--- src/index.js
+++ src/index.js
@@ -7,10 +7,24 @@ import { Provider } from "react-redux";
 import { createStore } from "redux";
 import rootreducer from "./reducers/rootReducer";
 
-const theStore = createStore(rootreducer);
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootreducer);
+const theStore = createStore(persistedReducer);
+const persistor = persistStore(theStore);
+
 ReactDOM.render(
   <Provider store={theStore}>
-    <App />
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>,
   document.getElementById("root")
 );
